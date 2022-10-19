7dc42117383511358938f9f61b8a072ddaa7c21a
Ikhideifidon <ikhideifidon@yahoo.com>
2022-10-19 13:07:52 -0400

    General

---
+++ src/main/java/com/Github/ikhideifidon/GeneralExercises.java
@@ -0,0 +1,2 @@
+package com.Github.ikhideifidon;public class GeneralExercises {
+}
---
+++ src/main/java/com/Github/ikhideifidon/Main.java
@@ -0,0 +1,5 @@
+package com.Github.ikhideifidon;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
