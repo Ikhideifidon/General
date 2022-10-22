c4e6672da7fe5b337fa64c2b51f1484d2c5c1765
Ikhideifidon <ikhideifidon@yahoo.com>
2022-10-22 12:42:07 -0400

    General

--- src/main/java/com/Github/ikhideifidon/GeneralExercises.java
+++ src/main/java/com/Github/ikhideifidon/GeneralExercises.java
@@ -2,7 +2,8 @@ package com.Github.ikhideifidon;
 
 
 public class GeneralExercises {
-
+    // 0/1 Knapsack Problem and Dynamic Programming
+    // Medium 416: Partition Equal Subset Sum
     public static boolean canPartition(int[] nums) {
         if (nums == null)
             return false;
@@ -34,7 +35,6 @@ public class GeneralExercises {
         if (total == 0)
             return true;
 
-
         if (table[start][total] == null) {
             if (nums[start] <= total) {
                 if (dp(nums, total - nums[start], start + 1, table)) {
@@ -45,5 +45,142 @@ public class GeneralExercises {
             table[start][total] = dp(nums, total, start + 1, table);
         }
         return table[start][total];
+    }
+
+    public static boolean canPartitionKSubsets(int[] nums, int k) {
+        if (nums == null)
+            return false;
+
+        if (nums.length == 1 && k == 1)
+            return true;
+
+        if (k > nums.length)
+            return false;
+
+        boolean[] visited = new boolean[nums.length];
+        int total = 0;
+        for (int num : nums)
+            total += num;
+
+        if (total % k != 0)
+            return false;
+
+        return helper(nums, 0, k, nums.length - 1, visited, total / k);
+
+    }
+
+    private static boolean helper(int[] nums, int total, int round, int start, boolean[] visited, int target) {
+        if (round == 0)
+            return true;
+
+        if (total == target && helper(nums, 0, round - 1, nums.length - 1, visited, target))
+            return true;
+
+        for (int i = start; i < nums.length; i++) {
+            if (!visited[i] && total + nums[i] <= target) {
+                visited[i] = true;
+                if (helper(nums, total + nums[i], round, i - 1, visited, target))
+                    return true;
+                visited[i] = false;
+            }
+        }
+        return false;
+    }
+
+    public static long fib(int n) {
+        if (n <= 1)
+            return n;
+
+        int result = 0;
+        int first = 0;
+        int second = 1;
+        for (int i = 2; i <= n; i++) {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+        return result;
+    }
+
+    // 0/1 Knapsack Problem
+    // Memoized
+    public static int solveKnapsack(int[] profits, int[] weights, int capacity) {
+        if (profits == null || weights == null)
+            throw new NullPointerException("Either or Both arrays cannot be null.");
+
+        int n = profits.length;   // Number of items given. n = weights.length is also correct.
+        if (n == 0)
+            return 0;
+
+        if (n == 1)
+            return weights[0] <= capacity ? profits[0] : 0;
+
+        // For n >= 2.
+        // For the recursive function, the only changing state variables are capacity and start.
+        // Both variables can be memoized.
+        int[][] memo = new int[n + 1][capacity + 1];
+        return knapsack(profits, weights, capacity, 0, memo);
+    }
+
+    private static int knapsack(int[] profits, int[] weights, int capacity, int start, int[][] memo) {
+        if (start >= profits.length)            // Recursive base case
+            return 0;
+
+        if (capacity <= 0)          // Backtracking
+            return 0;
+
+        // Check if the current start and capacity has been processed.
+        if (memo[start][capacity] != 0)
+            return memo[start][capacity];
+
+        // Decision to take
+        int profit1 = 0;
+        if (weights[start] <= capacity)
+            profit1 = profits[start] + knapsack(profits, weights, capacity - weights[start], start + 1, memo);
+
+        // Decision NOT to take
+        int profit2 = knapsack(profits, weights, capacity, start + 1, memo);
+
+        // Add the maximum between profit1 and profit2 to the memo
+        memo[start][capacity] = Math.max(profit1, profit2);
+
+        return memo[start][capacity];
+    }
+
+    public static int tabularKnapsack(int[] profits, int[] weights, int capacity) {
+        if (profits == null || weights == null)
+            throw new NullPointerException("Either or Both arrays cannot be null.");
+
+        int n = profits.length;   // Number of items given. n = weights.length is also correct.
+
+        if (capacity <= 0 || n == 0)
+            return 0;
+
+        if (n == 1)
+            return weights[0] <= capacity ? profits[0] : 0;
+
+        // n >= 2
+        int[][] dp = new int[n][capacity + 1];
+
+        // populate the capacity = 0 columns, with '0' since capacity we have '0' profit
+        for(int i = 0; i < n; i++)
+            dp[i][0] = 0;
+
+        // If we have only one weight, we will take it if it is not more than the given capacity.
+        for (int j = 0; j <= capacity; j++) {
+            if (weights[0] <= j)
+                dp[0][j] = profits[0];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= capacity; j++) {
+                int takeProfit = 0, notTakeProfit = 0;
+                if (weights[i] <= j)
+                    takeProfit = profits[i] + dp[i - 1][j - weights[i]];
+                else
+                    notTakeProfit = dp[i - 1][j];
+                dp[i][j] = Math.max(takeProfit, notTakeProfit);
+            }
+        }
+        return dp[n - 1][capacity];
     }
 }
---
+++ src/test/java/com/Github/ikhideifidon/GeneralExercisesTest.java
@@ -0,0 +1,68 @@
+package com.Github.ikhideifidon;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+class GeneralExercisesTest {
+
+    @Test
+    void canPartition() {
+    }
+
+    @Test
+    void canPartitionKSubsets() {
+    }
+
+    @Test
+    void fib() {
+    }
+
+    @Test
+    void solveKnapsack() {
+        int[] w = {54, 95, 36, 18, 4, 71, 83, 16, 27, 84, 88, 45, 94, 64, 14, 80, 4, 23, 75, 36,
+                90, 20, 77, 32, 58, 6, 14, 86, 84, 59, 71, 21, 30, 22, 96, 49, 81, 48, 37, 28, 6,
+                84, 19, 55, 88, 38, 51, 52, 79, 55, 70, 53, 64, 99, 61, 86, 1, 64, 32, 60, 42, 45,
+                34, 22, 49, 37, 33, 1, 78, 43, 85, 24, 96, 32, 99, 57, 23, 8, 10, 74, 59, 89, 95,
+                40, 46, 65, 6, 89, 84, 83, 6, 19, 45, 59, 26, 13, 8, 26, 5, 9};
+
+        int[] p = {297, 295, 293, 292, 291, 289, 284, 284, 283, 283, 281, 280, 279, 277, 276,
+                275, 273,264, 260, 257, 250, 236, 236, 235, 235, 233, 232, 232, 228, 218, 217,
+                214, 211, 208, 205, 204, 203, 201, 196, 194, 193, 193, 192, 191, 190, 187, 187,
+                184, 184, 184, 181, 179, 176, 173, 172, 171, 160, 128, 123, 114, 113, 107, 105,
+                101, 100, 100, 99, 98, 97, 94, 94, 93, 91, 80, 74, 73, 72, 63, 63, 62, 61, 60, 56,
+                53, 52, 50, 48, 46, 40, 40, 35, 28, 22, 22, 18, 15, 12, 11, 6, 5};
+
+        int C = 3818;
+
+
+        long startTime = System.currentTimeMillis();
+        int maxProfit = GeneralExercises.solveKnapsack(p, w, C);
+        Assertions.assertEquals(15170, maxProfit);
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Maximum Obtainable Profit is: " + maxProfit +"\nThis operation took " + (endTime - startTime) + " milliseconds");
+    }
+
+    @Test
+    public void knapsack() {
+        int[] w = {80,82,85,70,72,70,66,50,55,25,50,55,40,48,59,32,22, 60, 30, 32, 40, 38, 35,
+                32, 25, 28, 30, 22, 50, 30, 45, 30, 60, 50, 20, 65, 20, 25, 30, 10, 20, 25, 15,
+                10, 10, 10, 4, 4, 2, 1};
+
+        int[] p = {220, 208, 198, 192, 180, 180, 165, 162, 160, 158, 155, 130, 125, 122, 120,
+                118, 115, 110, 105, 101, 100, 100, 98, 96, 95, 90, 88, 82, 80, 77, 75, 73, 72,
+                70, 69, 66, 65, 63, 60, 58, 56, 50, 30, 20, 15, 10, 8, 5, 3, 1};
+
+        int C = 1000;
+        long startTime = System.currentTimeMillis();
+        int maxProfit = GeneralExercises.solveKnapsack(p, w, C);
+        Assertions.assertEquals(3103, maxProfit);
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Maximum Obtainable Profit is: " + maxProfit +"\nThis operation took " + (endTime - startTime) + " milliseconds");
+
+    }
+}
